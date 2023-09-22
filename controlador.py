fd26578fb675e9af31519e169502b6616bc66005
HJGH08 <145801904+HJGH08@users.noreply.github.com>
2023-09-22 16:22:41 -0600

    controlador.py

---
+++ controlador.py
@@ -0,0 +1,18 @@
+from odoo import models, fields
+
+class FingerprintRegistration(models.Model):
+    _name = 'fingerprint.registration'
+    _description = 'Fingerprint Registration'
+
+    name = fields.Char(string="Name", required=True)
+    fingerprint_data = fields.Binary(string="Fingerprint Data", required=True)
+    user_id = fields.Many2one('res.users', string="User", required=True)
+
+    # Otros campos y métodos del modelo
+
+    def unlink(self):
+        # Aquí puedes agregar lógica personalizada antes de eliminar el registro.
+        # Por ejemplo, puedes realizar una verificación o realizar alguna acción adicional.
+
+        # Llama al método original de eliminación para eliminar el registro
+        return super(FingerprintRegistration, self).unlink()
